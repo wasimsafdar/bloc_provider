5faf5e40b044b8980bd6ed7ed760889fdfd2e2a4
mac <mac@macs-mbp-3.home>
2022-03-07 21:44:42 +0500

    Fetch top rated movies using REST API

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:bloc_provider/src/ui/movie_list.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: MovieList(),
+      ),
+    );
+  }
+}
---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+import 'package:bloc_provider/src/models/trailer_model.dart';
+import 'movie_api_provider.dart';
+import '../models/item_model.dart';
+
+class Repository {
+  final moviesApiProvider = MovieApiProvider();
+
+  Future<ItemModel> fetchAllMovies() => moviesApiProvider.fetchMovieList();
+  Future<TrailerModel> fetchTrailers(int movieId) => moviesApiProvider.fetchTrailer(movieId);
+}
